ce036158cc88abce01050b185b3d14d3d290c379
Daniel Bengtsson <daniel.bengtson91@gmail.com>
2022-12-09 18:58:00 +0100

    First scanner prototype

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
+import Quagga from 'quagga';
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const handleStart = () => {
+    Quagga.init(
+      {
+        inputStream: {
+          name: 'Live',
+          type: 'LiveStream',
+          target: document.querySelector('#yourElement'), // Or '#yourElement' (optional)
+        },
+        decoder: {
+          readers: ['code_128_reader'],
+        },
+      },
+      function (err) {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log('Initialization finished. Ready to start');
+        Quagga.start();
+      }
+    );
+  };
+
+  const handleStop = () => {
+    Quagga.stop();
+  };
+
   return (
     <div className='App'>
-      <h1>Hello Scanner application</h1>
+      <div>
+        <button onClick={handleStart}>Start</button>
+        <button onClick={handleStop}>Stop</button>
+      </div>
+      <div id='yourElement'></div>
     </div>
   );
 }
