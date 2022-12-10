1a512b24ecb8c9109fd3c15de914d6b830e69121
Daniel Bengtsson <daniel.bengtson91@gmail.com>
2022-12-10 10:34:48 +0100

    Scanner update

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,58 @@
+import Quagga from 'quagga';
 import { useState } from 'react';
-import Scanner from './components/Scanner';
 import './App.css';
 
 function App() {
-  const [camera, setCamera] = useState(false);
-  const [result, setResult] = useState(null);
+  const [scanned, setScanned] = useState([]);
 
-  const onDetected = (result) => {
-    setResult(result);
+  const handleStart = () => {
+    Quagga.init(
+      {
+        inputStream: {
+          name: 'Live',
+          type: 'LiveStream',
+          target: document.querySelector('#yourElement'), // Or '#yourElement' (optional)
+        },
+        decoder: {
+          readers: ['ean_reader', 'ean_8_reader'],
+        },
+        area: {
+          // defines rectangle of the detection/localization area
+          top: '0%', // top offset
+          right: '0%', // right offset
+          left: '0%', // left offset
+          bottom: '0%', // bottom offset
+        },
+      },
+      function (err) {
+        if (err) {
+          console.log(err);
+        }
+        console.log('Initialization finished. Ready to start');
+        Quagga.start();
+      }
+    );
   };
-  console.log(result);
+
+  Quagga.onProcessed((callback) => {
+    console.log(callback);
+  });
+  Quagga.onDetected((result) => {
+    setScanned(result.codeResult.code);
+    console.log(result.codeResult.code);
+  });
+  const handleStop = () => {
+    Quagga.stop();
+    console.log(scanned);
+  };
+
   return (
     <div className='App'>
-      <p>{result ? result : 'Scanning...'}</p>
-      <button onClick={() => setCamera(!camera)}>
-        {camera ? 'Stop' : 'Start'}
-      </button>
-      <div className=''>{camera && <Scanner onDetected={onDetected} />}</div>
+      <div>
+        <button onClick={handleStart}>Start</button>
+        <button onClick={handleStop}>Stop</button>
+      </div>
+      <div id='yourElement'></div>
     </div>
   );
 }
