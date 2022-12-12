9757ef7658831e85da2372902f442fe75673659f
Daniel Bengtsson <57772634+Danneeb@users.noreply.github.com>
2022-12-12 13:18:44 +0000

    Added drawing around barcode

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,11 @@ function App() {
           name: 'Live',
           type: 'LiveStream',
           target: document.querySelector('#yourElement'), // Or '#yourElement' (optional)
-        },
+        },constraints: {
+          width: 100,
+          height: 20,
+          facing: "environment" // or user
+      },
         numOfWorkers: 1,
         decoder: {
           readers: ['ean_reader', 'ean_8_reader'],
@@ -37,6 +41,31 @@ function App() {
       Quagga.stop();
     });
   };
+
+  Quagga.onProcessed(function (result) {
+    var drawingCtx = Quagga.canvas.ctx.overlay,
+    drawingCanvas = Quagga.canvas.dom.overlay;
+
+    if (result) {
+        if (result.boxes) {
+            drawingCtx.clearRect(0, 0, parseInt(drawingCanvas.getAttribute("width")), parseInt(drawingCanvas.getAttribute("height")));
+            result.boxes.filter(function (box) {
+                return box !== result.box;
+            }).forEach(function (box) {
+                Quagga.ImageDebug.drawPath(box, { x: 0, y: 1 }, drawingCtx, { color: "green", lineWidth: 2 });
+            });
+        }
+
+        if (result.box) {
+            Quagga.ImageDebug.drawPath(result.box, { x: 0, y: 1 }, drawingCtx, { color: "#00F", lineWidth: 2 });
+        }
+
+        if (result.codeResult && result.codeResult.code) {
+            Quagga.ImageDebug.drawPath(result.line, { x: 'x', y: 'y' }, drawingCtx, { color: 'red', lineWidth: 3 });
+        }
+    }
+});
+
 
   const handleStop = () => {
     Quagga.stop();
